9974e5f994f87f1bc59f85234e666389a0fed580
amy skidmore <askidmore1@cnm.edu>
2017-04-13 11:03:27 -0600

    added presona

--- epic/use-case.php
+++ epic/use-case.php
@@ -2,9 +2,28 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
-	<title>Data Design Etsy</title>
+	<title><center>Data Design Etsy</center></title>
 </head>
-<body>
-
-</body>
+	<body><h1><strong>Data Design Using Etsy</h1></strong></body>
+	<h2>
+		<strong>Persona</strong>
+	</h2>
+	<p><strong>Name:</strong>Amber
+	<strong>Age:</strong>26
+	<strong>Profession:</strong>Artist
+	<strong>Technology:</strong>MacBooK Pro laptop, an Iphone Six Plus, and an Ipad Pro;all with unlimited data.
+	<strong>Attitudes:</strong>She works from home, doing things by her own schedule. She spends several hours per day online, including frequnetly browsing Etsy to update her art shop as well as to browse others shops for interesting items to add to her growing collection. She frequently uses social media to promote her gallary showings and the art shop she maintains on Etsy.
+	<strong>Frustrations and Needs:</strong>Amber is frustrated with the amout of traffic and sales her shop gets.She needs to increase traffic to her Etsy shop in-order to promote sales and recognition.
+	<strong>Goals:</strong>Amber goals include; increasing traffic to her shop, increasing sales from the shop, and increasing recoginition of her art.
+	</p>
+	<h2><strong>Use Case</strong>
+	</h2>
+	<p>A user favorites an Etsy shop by:
+	<li>The user accesses Etsy</li>
+	<li>The user selects the shop</li>
+	<li>The user clicks the favorites button</li></p>
+	<h2><strong>Conceptual Model</strong></h2>
+	<p><li>Many users can favorite many shops</li>
+	<li>Many shops can be favorite by many users</li>
+	</p>
 </html>
