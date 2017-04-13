b7b9c888b660b1d1b61bf2d0e6c75dd14dc3cf1c
amy skidmore <askidmore1@cnm.edu>
2017-04-13 11:22:18 -0600

    changes to persona and line breaks

--- epic/use-case.php
+++ epic/use-case.php
@@ -8,13 +8,13 @@
 	<h2>
 		<strong>Persona</strong>
 	</h2>
-	<p><strong>Name:</strong>Amber
-	<strong>Age:</strong>26
-	<strong>Profession:</strong>Artist
-	<strong>Technology:</strong>MacBooK Pro laptop, an Iphone Six Plus, and an Ipad Pro;all with unlimited data.
-	<strong>Attitudes:</strong>She works from home, doing things by her own schedule. She spends several hours per day online, including frequnetly browsing Etsy to update her art shop as well as to browse others shops for interesting items to add to her growing collection. She frequently uses social media to promote her gallary showings and the art shop she maintains on Etsy.
-	<strong>Frustrations and Needs:</strong>Amber is frustrated with the amout of traffic and sales her shop gets.She needs to increase traffic to her Etsy shop in-order to promote sales and recognition.
-	<strong>Goals:</strong>Amber goals include; increasing traffic to her shop, increasing sales from the shop, and increasing recoginition of her art.
+	<p><br><strong>Name:</strong> Amber</br>
+		<br><strong>Age:</strong> 26</br>
+		<br><strong>Profession:</strong> Artist</br>
+		<br><strong>Technology:</strong> MacBooK Pro laptop, an Iphone Six Plus, and an Ipad Pro;all with unlimited data.</br>
+		<br><strong>Attitudes:</strong> She works from home, doing things by her own schedule. She spends several hours per day online, including frequnetly browsing Etsy to update her art shop as well as to browse others shops for interesting items to add to her growing collection. She frequently uses social media to promote her gallary showings and the art shop she maintains on Etsy.</br>
+		<br><strong>Frustrations and Needs:</strong> Amber is frustrated with the amout of traffic and sales her shop gets.She needs to increase traffic to her Etsy shop in-order to promote sales and recognition.</br>
+		<br><strong>Goals:</strong> Amber goals include; increasing traffic to her shop, increasing sales from the shop, and increasing recoginition of her art.</br>
 	</p>
 	<h2><strong>Use Case</strong>
 	</h2>
@@ -24,6 +24,6 @@
 	<li>The user clicks the favorites button</li></p>
 	<h2><strong>Conceptual Model</strong></h2>
 	<p><li>Many users can favorite many shops</li>
-	<li>Many shops can be favorite by many users</li>
+	<li>Many shops can be favorited by many users</li>
 	</p>
 </html>
