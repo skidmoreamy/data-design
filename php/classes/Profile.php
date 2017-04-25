4257887e2866ae76774a673254cb87efdd0abd63
amy skidmore <askidmore1@cnm.edu>
2017-04-25 10:14:50 -0600

    checkpoint commit

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -70,9 +70,10 @@ class Profile implements \JsonSerializable{
 		$hash = hash_pbkdf2("sha512",$password, $salt,262144);
 	}
 /**
- * accessor metho for profile id
+ * accessor method for profile id
  */
-public function getprofileId() :?int{return($this->profileId);}
+public function getprofileId() :?int{return
+	($this->profileId);}
  /**
   * mutator method for profileid
   * @param int/null $newprofileId is not positive
@@ -81,9 +82,107 @@ public function getprofileId() :?int{return($this->profileId);}
   */
 	/**
 	 * @param int $profileId
-	 */
-	public function setProfileId(int $profileId) {
-		$this->profileId = $profileId;
+	 **/
+	public function setProfileId(int $newprofileId) :
+	void{
+	if($newprofileId === null) {
+		$this->profileId = null;
+		return;
 	}
-	
+	if ($newprofileId <= 0) {
+		throw(new \RangeException("profile id is not prositive"));
+	}
+	$this->profileId = $newprofileId;
+	}
+	public function getprofileId () : int{
+		return($this->profileId));
+	}
+	public function setprofileId(int$newprofileId) : void {
+		if ($newprofileId <= 0){
+			throw(new \RangeException("profile id is not positive"));
+		}
+		$this->profileId = $newprofileId;
+	}
+	public static function getprofilebyprofileId(\PDO $pdo, int $profileId): \SPLFixedArray {
+		if($profileIdId <= 0) {
+			throw(new \RangeException("profile id must be positive"));
+		}
+		$query = "SELECT profileId, shopProfileId, favorite FROM profile WHERE"
+
+	/** inserts this into mySQL
+	 */
+	 public function insert(\PDO $pdo): void {
+			//enforce the profileId is null (i.e., don't insert a profile that already exists)
+			if($this->profileId !== null) {
+				throw(new\PDOException ("not a new profile"));
+			}
+		}
+			/**
+			 * create query template
+			 */
+			/
+
+			$query = "INSERT INTO profileId(profileActivationToken, profileEmail) VALUES (:profileActivationToken, :profileEmail)";
+			$statement = $pdo->prepare($query);
+		}
+
+	// bind the member variable to the place holders in the template
+		/**  deletes this profile from mySQL.
+		 */
+	public function delete(PDO $pdo) : void{
+		// enforce the profileId is not null
+			if($this->profileId === null){
+			throw(new \PDOException ("unable to delete a profile that does not exist"));
+			}
+			//create query template
+			$query = "DELETE FROM profile WHERE profileId = :profileId";
+			$sttement = $pdo->prepare($query);
+			// bind the member variables to the place holder in the template
+			$parameters + ["profileId" => $this->profileId];
+			$sttement->execute($parameters);
+		}
+
+
+		//updates this into mySQL
+		public function update(\PDO $pdo) : void {
+	//enforce the profileId is not null (i.e., don't update a profile that hasn't been inserted)
+			if (this->profileId === null{
+				throw(new \PDOExcetpion("unable to update a profile that does not exist"));
+			}
+			//create a query table
+			$query = "UPDATE profile SET profileId = : profileId, profileActivationToken = : profileActivationToken, 
+profileEmail = : profileEmail WHERE profileId = :profileId";
+			$statement = $pdo->prepare($query);
+			//bind the member variables to the place holders in the template
+
+
+			//gets the ____ by content
+
+			//gets the profile by profileId
+			public static function getProfileByProfileId(PDO $pdo, int $profileId) :?Profile {
+				//sanitize the profileid befor searching
+				if($profileId <= 0){
+					throw(new \PDOException("profile id is not positive"));
+				}
+			//create query template
+				$query = "SELECT profile "
+			}
+			}
 }
+
+
+
+
+
+
+
+
+
+
+	$query = "INSERT INTO shop (shopContent, shopName) VALUES (:shopContent, : shopName)";
+	$statement = $pdo->prepare($query);
+
+
+
+
+	}
